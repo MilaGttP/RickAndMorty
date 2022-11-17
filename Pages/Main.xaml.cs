8ec47b7087c01e8085355c19c75971ce735c1753
Mila <milaivanova2609@gmail.com>
2022-11-17 18:23:44 +0200

    Main page completed

--- Pages/Main.xaml.cs
+++ Pages/Main.xaml.cs
@@ -30,16 +30,9 @@ namespace RickAndMorty
 
         private void Main_Loaded(object sender, RoutedEventArgs e)
         {
-            Character JerrySmith = new Character();
-            JerrySmith = JsonWork.GetCharacter(ref characters, 5);
-            JerryName.Text = JerrySmith.Name;
-            JerryStatus.Text = $"{JerrySmith.Status} - {JerrySmith.Species}\n";
-            JerryLastLoc.Text = $"{JsonWork.GetLocationForCharacter(ref characters, 5)}\n";
-            JerryFSeen.Text = JsonWork.GetFirstEpisodeForCharacter(ref characters, 5);
-
             Character Rick = new Character();
             Rick = JsonWork.GetCharacter(ref characters, 1);
-            RickName.Text = Rick.Name;  
+            RickName.Text = Rick.Name;
             RickStatus.Text = $"{Rick.Status} - {Rick.Species}\n";
             RickLastLoc.Text = $"{JsonWork.GetLocationForCharacter(ref characters, 1)}\n";
             RickFSeen.Text = JsonWork.GetFirstEpisodeForCharacter(ref characters, 1);
@@ -50,6 +43,34 @@ namespace RickAndMorty
             MortyStatus.Text = $"{Morty.Status} - {Morty.Species}\n";
             MortyLastLoc.Text = $"{JsonWork.GetLocationForCharacter(ref characters, 2)}\n";
             MortyFSeen.Text = JsonWork.GetFirstEpisodeForCharacter(ref characters, 2);
+
+            Character Summer = new Character();
+            Summer = JsonWork.GetCharacter(ref characters, 3);
+            SummerName.Text = Summer.Name;
+            SummerStatus.Text = $"{Summer.Status} - {Summer.Species}\n";
+            SummerLastLoc.Text = $"{JsonWork.GetLocationForCharacter(ref characters, 3)}\n";
+            SummerFSeen.Text = JsonWork.GetFirstEpisodeForCharacter(ref characters, 3);
+
+            Character Beth = new Character();
+            Beth = JsonWork.GetCharacter(ref characters, 4);
+            BethName.Text = Beth.Name;
+            BethStatus.Text = $"{Beth.Status} - {Beth.Species}\n";
+            BethLastLoc.Text = $"{JsonWork.GetLocationForCharacter(ref characters, 4)}\n";
+            BethFSeen.Text = JsonWork.GetFirstEpisodeForCharacter(ref characters, 4);
+
+            Character JerrySmith = new Character();
+            JerrySmith = JsonWork.GetCharacter(ref characters, 5);
+            JerryName.Text = JerrySmith.Name;
+            JerryStatus.Text = $"{JerrySmith.Status} - {JerrySmith.Species}\n";
+            JerryLastLoc.Text = $"{JsonWork.GetLocationForCharacter(ref characters, 5)}\n";
+            JerryFSeen.Text = JsonWork.GetFirstEpisodeForCharacter(ref characters, 5);
+
+            Character Abradolf = new Character();
+            Abradolf = JsonWork.GetCharacter(ref characters, 7);
+            AbradolfName.Text = Abradolf.Name;
+            AbradolfStatus.Text = $"{Abradolf.Status} - {Abradolf.Species}\n";
+            AbradolfLastLoc.Text = $"{JsonWork.GetLocationForCharacter(ref characters, 7)}\n";
+            AbradolfFSeen.Text = JsonWork.GetFirstEpisodeForCharacter(ref characters, 7);
         }
     }
 }
